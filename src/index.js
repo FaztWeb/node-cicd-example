d6618b8bf983eccae2769c8fb4f1fbd3c1f07fbd
fazttech <fazt@faztweb.com>
2020-10-30 23:19:26 -0500

    ci/cd first try

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World From CI/CD");
 });
 
 app.listen(app.get("port"));

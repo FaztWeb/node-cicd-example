ed67311c57eaef863e906fbe0a02f2637c04eaed
fazttech <fazt@faztweb.com>
2020-10-31 16:40:48 -0500

    hello world

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  res.send("Hello World Updated With Hooks!");
+  res.send("Hello World!");
 });
 
 app.listen(app.get("port"));

ee7ba0c48c686e4a4173742f3b57e27bffc8c670
fazttech <fazt@faztweb.com>
2020-10-31 17:03:29 -0500

    hello world again

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  res.send("Hello World With Jenkins and Github Actions!");
+  res.send("Hello World!");
 });
 
 app.listen(app.get("port"));

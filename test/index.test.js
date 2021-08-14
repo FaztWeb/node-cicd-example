503edb4d5ab8ea6b13c37c3a57f2ac5ced7efa03
fazt <fazt@faztweb.com>
2021-08-14 15:00:19 -0500

    some changed made

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ const app = require("../src/index");
 
 describe("GET /", () => {
   it('should respond with "Hello World"', (done) => {
-    request(app).get("/").expect("Hello World!", done);
+    request(app).get("/").expect("Hello World!!", done);
   });
 });
